4a322ea0d70f94b0b773efb19ffeefb4ff38053e
Vendoraiq <vendoraiq@gmail.com>
2025-05-08 22:40:02 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-# App logic here
+from flask import Flask, request, jsonify
+from utils.merged_logic import get_amazon_data
+
+app = Flask(__name__)
+
+@app.route("/analyze", methods=["POST"])
+def analyze():
+    data = request.get_json()
+    asin = data.get("asin")
+    return jsonify(get_amazon_data(asin))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
